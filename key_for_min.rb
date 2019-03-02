cd3d9827162748867b3f0c53c69f8cb34dfa4e8e
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-02 23:35:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   low_k = nil 
   low_v = nil 
   name_hash.each do |k, v|
-    if low_v = nil || v < low_v
+    if low_v == nil || v < low_v
       low_v = v
       low_k = k
     end
